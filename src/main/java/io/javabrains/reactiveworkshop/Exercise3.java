b09e26c88cac83e55c5c4808a5fc98cd7fe7f228
Bartek <bl4kee@wp.pl>
2022-07-27 20:29:53 +0200

    Exercise3 solutions.

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,20 +1,23 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumbersFlux()
+    // Use ReactiveSources.intNumbersFlux()
 
-        // Get all numbers in the ReactiveSources.intNumbersFlux stream
-        // into a List and print the list and its size
-        // TODO: Write code here
+    // Get all numbers in the ReactiveSources.intNumbersFlux stream
+    // into a List and print the list and its size
+    List<Integer> numbers = ReactiveSources.intNumbersFlux().collectList().block();
+    numbers.forEach(System.out::println);
+    System.out.println("List size: " + numbers.size());
 
 
-        System.out.println("Press a key to end");
-        System.in.read();
-    }
+    System.out.println("Press a key to end");
+    System.in.read();
+  }
 
 }
