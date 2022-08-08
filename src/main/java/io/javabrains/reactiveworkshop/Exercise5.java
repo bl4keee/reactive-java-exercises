990589655e55494c4c18cdaa53419cc11ae83ad4
Bartek <bl4kee@wp.pl>
2022-08-08 21:16:58 +0200

    Exercise5 solutions.

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -4,18 +4,21 @@ import java.io.IOException;
 
 public class Exercise5 {
 
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+    // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
-        // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+    // Subscribe to a flux using the error and completion hooks
+    ReactiveSources.intNumberMono().subscribe(
+        number -> System.out.println(number),
+        error -> System.out.println(error.getMessage()),
+        () -> System.out.println("Completed!"));
 
-        // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+    // Subscribe to a flux using an implementation of BaseSubscriber
+    // TODO
 
-        System.out.println("Press a key to end");
-        System.in.read();
-    }
+    System.out.println("Press a key to end");
+    System.in.read();
+  }
 
 }
